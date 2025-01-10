543b8a82cad483a1a087e6a59d411a44cf26f598
Tiago Antunes <tiagoantunes9991@gmail.com>
2025-01-10 01:37:30 +0000

    asdasd

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_01_10_001951) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "properties", force: :cascade do |t|
     t.string "title"
     t.text "description"
